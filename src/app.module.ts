8224810bf507f1bc8fd59905612b2f83e9e06065
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-07-14 08:14:32 +0900

    db setting

---
+++ src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProfileService } from './profile/profile.service';
+import { ProfileController } from './profile/profile.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { Profile } from './profile/entity/profile.entity';
+import { ProfileModule } from './profile/profile.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('mysql').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Profile],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+    ProfileModule,
+  ],
+  controllers: [AppController, ProfileController],
+  providers: [AppService, ProfileService],
+})
+export class AppModule {}
